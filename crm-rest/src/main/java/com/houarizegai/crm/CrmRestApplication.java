5195d789828412eaa868509da49c4afbaae93d31
HouariZegai <houarizegai14@gmail.com>
2019-09-02 23:26:02 +0100

    Back-end - Init SpringBoot Project [Restful WS]

---
+++ crm-rest/src/main/java/com/houarizegai/crm/CrmRestApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmRestApplication.class, args);
+	}
+
+}
