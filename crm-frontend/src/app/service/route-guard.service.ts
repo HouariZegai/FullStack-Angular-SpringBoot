aacdc3e00accd2fde9d11f3da15d608a6962e0e9
HouariZegai <houarizegai14@gmail.com>
2019-09-02 16:05:32 +0100

    Front-end - auto redirect to login page when it's not login

--- crm-frontend/src/app/service/route-guard.service.ts
+++ crm-frontend/src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
@@ -7,9 +7,13 @@ import { HardcodedAuthenticationService } from './hardcoded-authentication.servi
 })
 export class RouteGuardService implements CanActivate {
   
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.hardcodedAuthenticationService.isUserLoggedIn();
+    if(this.hardcodedAuthenticationService.isUserLoggedIn())
+      return true;
+
+    this.router.navigate(['login']);
+    return false;
   }
 }
